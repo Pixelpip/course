928aa4abc746a50d80e1bed006dc114351f7ae0a
Borat560 <athulkamathg5@gmail.com>
2023-07-18 23:18:12 +0530

    kommit

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -6,7 +6,10 @@ public class SecondsInADay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int d=Integer.valueOf(scanner.nextLine());
+        int a=d*24*60*60;
+        System.out.println(a);
 
     }
 }
