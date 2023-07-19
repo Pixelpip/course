7a11a9f91071284ee3e54ee690b7f6db31822ded
Borat560 <athulkamathg5@gmail.com>
2023-07-19 18:35:24 +0530

    commit

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_17.SumOfTwoNumbers/src/main/java/SumOfTwoNumbers.java
@@ -10,10 +10,9 @@ public class SumOfTwoNumbers {
         int a=Integer.valueOf(scanner.nextLine());
         System.out.println("Give the second number:");
         int b=Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the third number:");
-        int c=Integer.valueOf(scanner.nextLine());
-        int d=a+b+c;
-        System.out.println("The sum of the number is "+d);
+        
+        int c=a+b;
+        System.out.println("The sum of the number is "+c);
 
     }
 }
