7020b7708230398f58446318a08ade8564bc7a86
Borat560 <athulkamathg5@gmail.com>
2023-07-19 20:08:35 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -6,7 +6,16 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number: ");
+        int a=Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        int b=Integer.valueOf(scanner.nextLine());
+        
+        double d=a/b;
+        System.out.println(a+" + "+b+" = "+(a+b));
+        System.out.println(a+" - "+b+" = "+(a-b));
+        System.out.println(a+" * "+b+" = "+(a*b));
+        System.out.println(a+" / "+b+" = "+d);
 
     }
 }
