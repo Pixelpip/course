df0f8dcc9728bff583208f80a3f2fd7b2a0c038c
Borat560 <athulkamathg5@gmail.com>
2023-07-19 18:43:36 +0530

    kommit

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
@@ -6,7 +6,13 @@ public class SumOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-
+        System.out.println("Give the first number: ");
+        int a=Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        int b=Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number: ");
+        int c=Integer.valueOf(scanner.nextLine());
+        int d=a+b+c;
+        System.out.println("The sum of the numbers is "+d);
     }
 }
