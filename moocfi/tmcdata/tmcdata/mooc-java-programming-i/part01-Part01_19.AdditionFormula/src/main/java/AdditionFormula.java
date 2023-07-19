ba881b7709d561eb1b503841d2e319415c32d677
Borat560 <athulkamathg5@gmail.com>
2023-07-19 18:52:06 +0530

    k

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
@@ -6,7 +6,11 @@ public class AdditionFormula {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // write your program here
+        System.out.println("Give the first number: ");
+        int a=Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        int b=Integer.valueOf(scanner.nextLine());
+        System.out.println(a+" + "+b+" = "+(a+b));
 
     }
 }
