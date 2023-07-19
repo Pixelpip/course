969dfa7a77b49a9ab30d2b409f91245a3ad521fc
Borat560 <athulkamathg5@gmail.com>
2023-07-19 19:19:34 +0530

    k

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -6,7 +6,13 @@ public class AverageOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int a=Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int b=Integer.valueOf(scanner.nextLine());
+        System.out.println("The average is "+((a+b)/b));
+
+        
 
     }
 }
