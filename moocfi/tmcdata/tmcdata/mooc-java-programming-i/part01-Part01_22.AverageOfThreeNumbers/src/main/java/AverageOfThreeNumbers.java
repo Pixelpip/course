daeacd68ac874995a68383dbba1e5e10835a0554
Borat560 <athulkamathg5@gmail.com>
2023-07-19 19:36:07 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_22.AverageOfThreeNumbers/src/main/java/AverageOfThreeNumbers.java
@@ -6,7 +6,16 @@ public class AverageOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number: ");
+        int a=Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number: ");
+        int b=Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number: ");
+        int c=Integer.valueOf(scanner.nextLine());
+        double d=(a+b+c)/3.0;
+        System.out.println("The average is "+d);
+        
+        
 
     }
 }
