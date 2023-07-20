8f80d312c34a58ff8b603dff62637ee72fc6e7a7
Borat560 <athulkamathg5@gmail.com>
2023-07-20 19:05:40 +0530

    k

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 public class SpeedingTicket {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+      Scanner s=new Scanner(System.in);
+      System.out.println("Give speed:");
+      int a=Integer.valueOf(s.nextLine());
+      if(a>120)
+      {
+         System.out.println("Speeding ticket!");
+      }
 
-        // Write your program here. 
+
+        
     }
 }
--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_25.CheckYourIndentation/src/main/java/CheckYourIndentation.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_25.CheckYourIndentation/src/main/java/CheckYourIndentation.java
@@ -10,8 +10,12 @@ System.out.println("Give a number: ");
 int first = Integer.valueOf(scan.nextLine());
 System.out.println("Give another number: ");
 int second = Integer.valueOf(scan.nextLine());
-if (first == second) { System.out.println("Same!"); }
-else if (first > second) { System.out.println("The first was larger than the second!"); }
+if (first == second) { 
+    System.out.println("Same!");
+ }
+else if (first > second) { 
+    System.out.println("The first was larger than the second!"); 
+}
 else {
 System.out.println("The second was larger than the first!");
 }
