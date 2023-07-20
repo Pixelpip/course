3695cd5fadc9d1641cfbe65f24f778b423824840
Borat560 <athulkamathg5@gmail.com>
2023-07-20 23:08:00 +0530

    k

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_34.Same/src/main/java/Same.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_34.Same/src/main/java/Same.java
@@ -6,6 +6,15 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here. 
+        System.out.println("Enter the first string:");
+        String s=scan.nextLine();
+        System.out.println("Enter the second string:");
+        String m=scan.nextLine();
+        if(s.equals(m)){
+            System.out.println("Same");
+
+        }else{
+            System.out.println("Different");
+        }
     }
 }
