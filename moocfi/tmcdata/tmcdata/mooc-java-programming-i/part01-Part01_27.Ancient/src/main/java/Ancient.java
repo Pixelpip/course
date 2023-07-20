81ecee3f486e6b0afb763f5b618b8b6503ea1e1b
Borat560 <athulkamathg5@gmail.com>
2023-07-20 19:21:24 +0530

    k

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -6,6 +6,12 @@ public class Ancient {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give a year: ");
+        int a=Integer.valueOf(scan.nextLine());
+        if(a<2015)
+        {
+            System.out.println("Ancient history!");
+
+        }
     }
 }
