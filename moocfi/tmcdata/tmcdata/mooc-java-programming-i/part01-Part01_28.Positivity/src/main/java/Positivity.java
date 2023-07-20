1cec49c08a79c7b870b78a6c20ea56a1c8ce7f05
Borat560 <athulkamathg5@gmail.com>
2023-07-20 22:03:35 +0530

    k

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -7,6 +7,17 @@ public class Positivity {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+
+        System.out.println("Give a number: ");
+        int a=Integer.valueOf(scan.nextLine());
+        if(a>=1){
+            System.out.println("The number is positive.");
+
+        }
+        else{
+            System.out.println("The number is not positive.");
+
+        }
         
     }
 }
