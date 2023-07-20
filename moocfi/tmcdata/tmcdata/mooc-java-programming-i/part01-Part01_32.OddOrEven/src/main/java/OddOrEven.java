24d833f8e3173204b4213b95bd1661b521e6bf0d
Borat560 <athulkamathg5@gmail.com>
2023-07-20 22:47:48 +0530

    k

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
@@ -6,19 +6,12 @@ public class OddOrEven {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
-        // HINT:
-        // You can find out if a number is even or odd easily using the modulo operator %
-        // Try the following commands to see what they print
-        // System.out.println( 1%2 );
-        // System.out.println( 2%2 );
-        // System.out.println( 3%2 );
-        // System.out.println( 4%2 );
-        // System.out.println( 5%2 );
-        // System.out.println( 6%2 );
-        // System.out.println( 7%2 );
-        // int luku = 8
-        // System.out.println( luku%2 );
-        // So, by taking the modulo of a number and two you can find out if it is even or odd !
+        System.out.println("Enter a number");
+        int a=Integer.valueOf(scan.nextLine());
+        if(a%2==0){
+            System.out.println("Number "+a+" is even.");
+        }else{
+            System.out.println("Number "+a+" is odd.");
+        }
     }
 }
