035c08d4403fbd51ea000d824d20071f7868ca04
Borat560 <athulkamathg5@gmail.com>
2023-07-21 23:55:20 +0530

    co

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -7,17 +7,12 @@ public class LeapYear {
         Scanner scan = new Scanner(System.in);
         System.out.println("Give a year:");
         int a=Integer.valueOf(scan.nextLine());
-        if((a%4==0)){
+        if((a%4==0&&a%100!=0)){
             System.out.println("The year is a leap year.");
-        }
-        else if(a%100==0&&a%400==0){
-            System.out.println("The year is a leap year.");
-        }else if(a%400==0){
+        }else if(a%400==0&&a%100==0){
             System.out.println("The year is a leap year.");
         }else{
             System.out.println("The year is not a leap year.");
         }
-    
-        
     }
 }
