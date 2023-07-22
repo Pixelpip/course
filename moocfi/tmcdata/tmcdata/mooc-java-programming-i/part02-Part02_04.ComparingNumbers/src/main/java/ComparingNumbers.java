e575a821637dee4139751e73434f4bb089c02ccc
Borat560 <athulkamathg5@gmail.com>
2023-07-22 20:16:18 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -5,6 +5,16 @@ public class ComparingNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a=Integer.valueOf(scanner.nextLine());
+        int b=Integer.valueOf(scanner.nextLine());
+        if(a>b){
+            System.out.println(a+" is greater than "+b);
+        }else if(a<b){
+            System.out.println(a+" is smaller than "+b);
+        }else{
+            System.out.println(a+" is equal to "+b);
+        }
+
 
     }
 }
