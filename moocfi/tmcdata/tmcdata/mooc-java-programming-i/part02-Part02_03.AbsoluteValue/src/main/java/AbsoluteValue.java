a24d58fe8cc6f51adf26eef5b04772be39f0128c
Borat560 <athulkamathg5@gmail.com>
2023-07-22 20:00:14 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,7 +5,12 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int a=Integer.valueOf(scanner.nextLine());
+        if(a<0){
+            System.out.println(a*-1);
+        }else{
+            System.out.println(a);
+        }
 
     }
 }
