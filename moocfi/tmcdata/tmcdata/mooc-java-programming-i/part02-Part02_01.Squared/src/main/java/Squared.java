6854dc55067b9fd4630720ab935436ec96ca02c4
Borat560 <athulkamathg5@gmail.com>
2023-07-22 18:58:49 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,8 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a=Integer.valueOf(scanner.nextLine());
+        System.out.println((a*a));
 
     }
 }
