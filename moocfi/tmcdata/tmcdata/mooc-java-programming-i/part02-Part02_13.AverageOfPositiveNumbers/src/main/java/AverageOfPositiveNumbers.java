c8e19ef324ddb160024b8c9600dc80bc51c00dbd
Borat560 <athulkamathg5@gmail.com>
2023-07-23 23:43:41 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -4,7 +4,30 @@ import java.util.Scanner;
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+            int i=0;
+            double n=0;
+            while(true){
+                System.out.println("Give a number:");
+                int a=Integer.valueOf(scanner.nextLine());
+                if(a!=0){
+                    if(a>0){
+                        i=i+a;
+                        n++; 
 
+                      
+                    }
+                    continue;
+                }else if(n==0&&a==0){
+                    System.out.println("Cannot calulate the average");
+                    break;
+                }else if(a==0){
+                    break;
+                }
+            }
+            System.out.println(i/n);
+    
+        }
     }
-}
+    
+
