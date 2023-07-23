23fb091c3aa8fcf9335bf6bf22dcb8d0641f82aa
Borat560 <athulkamathg5@gmail.com>
2023-07-23 21:49:10 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -5,6 +5,15 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Give a number:");
+            int a=Integer.valueOf(scanner.nextLine());
+            if(a==4){
+                break;
+            }
+            
+            
+        }
 
     }
 }
