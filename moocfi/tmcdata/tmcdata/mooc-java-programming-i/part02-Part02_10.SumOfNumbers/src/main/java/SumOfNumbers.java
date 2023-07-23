da74e141dbbca200e32fd5f81bc304ccba364ec6
Borat560 <athulkamathg5@gmail.com>
2023-07-23 23:28:29 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,17 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i=0;
+        while(true){
+            System.out.println("Give a number:");
+            int a=Integer.valueOf(scanner.nextLine());
+            if(a!=0){
+                i=i+a;
+            }else {
+                break;
+            }
+        }
+        System.out.println("Sum of the numbers: "+i);
 
     }
 }
