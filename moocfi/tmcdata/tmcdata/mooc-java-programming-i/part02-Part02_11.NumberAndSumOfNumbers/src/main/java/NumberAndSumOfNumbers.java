e12399f9c1a19dfb11b96859894e6ac569b3f01d
Borat560 <athulkamathg5@gmail.com>
2023-07-23 23:36:27 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -1,10 +1,24 @@
 
 import java.util.Scanner;
 
-public class NumberAndSumOfNumbers {
+public class NumberAndSumOfNumbers{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i=0;
+        int n=0;
+        while(true){
+            System.out.println("Give a number:");
+            int a=Integer.valueOf(scanner.nextLine());
+            if(a!=0){
+                i=i+a;
+                n++;
+            }else {
+                break;
+            }
+        }
+        System.out.println("Number of numbers: "+n);
+        System.out.println("Sum of the numbers: "+i);
 
     }
 }
--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,20 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i=0;
+        double n=0;
+        while(true){
+            System.out.println("Give a number:");
+            int a=Integer.valueOf(scanner.nextLine());
+            if(a!=0){
+                i=i+a;
+                n++;
+            }else {
+                break;
+            }
+        }
+        System.out.println("Average of the numbers: "+(i/n));
+        
 
     }
 }
