fc99cf1b25134a7865f629cd9ddf896607191d37
Borat560 <athulkamathg5@gmail.com>
2023-07-23 22:12:43 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -5,7 +5,19 @@ public class OnlyPositives {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        while(true){
+            System.out.println("Give a number:");
+            int a=Integer.valueOf(scanner.nextLine());
+            if(a==0){
+                break;
+            }else if(a<0){
+                System.out.println("Unsuitable number");
+            }else{
+                System.out.println(a*a);
+                
+
+            }
+        }
 
     }
 }
