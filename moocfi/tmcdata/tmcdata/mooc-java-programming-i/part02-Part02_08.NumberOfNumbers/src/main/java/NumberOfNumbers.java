7160dce5d37d81ce31368a816127bc5710011ddc
Borat560 <athulkamathg5@gmail.com>
2023-07-23 22:51:19 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -1,10 +1,21 @@
-
 import java.util.Scanner;
 
 public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i=0;
+        System.out.println("Give a number:");
+        while(true){
+          
+            int a=Integer.valueOf(scanner.nextLine());
+            if(a==0){
+                break;
+            }if(a!=0){
+                i=i+1;
+            }
+        }
+        System.out.println("Number of numbers:"+i);
 
     }
 }
