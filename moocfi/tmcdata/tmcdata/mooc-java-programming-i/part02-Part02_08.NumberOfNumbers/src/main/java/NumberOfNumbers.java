9fc3c3fb5ffe52a53e1b75ce89bc546729a17a16
Borat560 <athulkamathg5@gmail.com>
2023-07-23 23:00:54 +0530

    deleted the not needed if

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,17 +5,14 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int i=0;
-        
         while(true){
             System.out.println("Give a number:");
             int a=Integer.valueOf(scanner.nextLine());
             if(a==0){
                 break;
-            }if(a!=0){
-                i=i+1;
             }
-        }
-        System.out.println("Number of numbers: "+i);
+            i=i+1;
+        }System.out.println("Number of numbers: "+i);
 
     }
 }
