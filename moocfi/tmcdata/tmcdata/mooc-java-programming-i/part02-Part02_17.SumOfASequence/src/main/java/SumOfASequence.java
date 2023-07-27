ef1e26d7429eb587d91b56b81c54793aab8da8f8
Borat560 <athulkamathg5@gmail.com>
2023-07-27 22:43:23 +0530

    msg

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,14 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Last number?");
+        int a=Integer.valueOf(scanner.nextLine());
+        int r=0;
+        for(int i=1;i<=a;i++){
+            r=r+i;
+            
+        }
+        System.out.println("The sum is "+r);
 
     }
 }
