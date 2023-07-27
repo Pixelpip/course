25c5c005bcc3f6204b01c6223c8189b17532ff0c
Borat560 <athulkamathg5@gmail.com>
2023-07-27 21:41:22 +0530

    msg

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_14.Counting/src/main/java/Counting.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,12 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a=Integer.valueOf(scanner.nextLine());
+        for(int i=0;i<=a;i++){
+            System.out.println(i);
+        }
+
+        
 
     }
 }
