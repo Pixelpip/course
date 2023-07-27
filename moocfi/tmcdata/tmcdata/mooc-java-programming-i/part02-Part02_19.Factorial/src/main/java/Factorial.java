ba563e14798af23c203f8c6d064a9283ba1d83b9
Borat560 <athulkamathg5@gmail.com>
2023-07-27 23:00:51 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,16 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int a=Integer.valueOf(scanner.nextLine());
+        int r=1;
+        for(int i=1;i<=a;i++){
+            r=r*i;
+            
+        }
+        System.out.println("Factorial "+r);
 
     }
+
 }
+
