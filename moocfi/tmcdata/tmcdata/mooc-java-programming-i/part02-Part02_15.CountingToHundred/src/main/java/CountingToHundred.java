84c24dcad30a4ce4ade17fe5a5f881cc7506be2c
Borat560 <athulkamathg5@gmail.com>
2023-07-27 22:09:21 +0530

    c

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,10 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a=Integer.valueOf(scanner.nextLine());
+        for(int i=a;i<=100;i++){
+            System.out.println(i);
+        }
 
     }
 }
--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -5,7 +5,23 @@ public class FromWhereToWhere {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+        System.out.println("Where to?");
+        int a=Integer.valueOf(scanner.nextLine());
+        //for(int i=1;i<=a;i++){
+           //System.out.println(i);
+        //}
+        System.out.println("Where from?");
+        int b=Integer.valueOf(scanner.nextLine());
+        if(b>a)
+        {
+            System.out.println("");
+        }
+        else
+        {
+           for(int i=b;i<=a;i++)
+           {
+            System.out.println(i);
+           }
+        }
     }
 }
