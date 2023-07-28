27bf17bbf85fdc9fb238eac329847cce323db7f2
Borat560 <athulkamathg5@gmail.com>
2023-07-28 19:54:59 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner;
 
 public class FromOneToParameter {
 
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=Integer.valueOf(s.nextLine());
+        printUntilNumber(a);
 
+    }
+    public static void printUntilNumber(int number){
+        int i=1;
+        while(i<=number){
+            System.out.println(i);
+            i++;
+        }
     }
 
 }
