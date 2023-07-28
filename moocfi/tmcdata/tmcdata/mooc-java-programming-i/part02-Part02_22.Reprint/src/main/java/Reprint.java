d2833d38bd239171d4c8eedfcf373bdf64a0434b
Borat560 <athulkamathg5@gmail.com>
2023-07-28 19:35:42 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_22.Reprint/src/main/java/Reprint.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -8,11 +8,19 @@ public class Reprint {
         // then call the printText-method multiple times with a while-loop
         
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many times?");
+        int a=Integer.valueOf(scanner.nextLine());
+        int n=0;
+        while(n<a){
+            printText();
+            n++;
+        }
     }
     
     // Don't change the next line that defines the method!
     // (We aren't giving a method a parameter yet)
     public static void printText() {
         // write some code here
+        System.out.println("In a hole in the ground there lived a method");
     }
 }
