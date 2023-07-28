5fb7f2d1a6dff5737a03da6e0b9269cbd2248809
Borat560 <athulkamathg5@gmail.com>
2023-07-28 20:04:33 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -1,8 +1,19 @@
-
+import java.util.Scanner;
 
 public class FromParameterToOne {
 
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=Integer.valueOf(s.nextLine());
+        printFromNumberToOne(a);
+
+    }
+    public static void printFromNumberToOne(int number){
+        
+        while(number>=1){
+            System.out.println(number);
+            number--;
+        }
 
 
     }
