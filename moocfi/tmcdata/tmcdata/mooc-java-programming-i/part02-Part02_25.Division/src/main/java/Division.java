3fe679573788722bd2568e3f9fee1abed095d74d
Borat560 <athulkamathg5@gmail.com>
2023-07-28 22:32:29 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
@@ -1,8 +1,12 @@
-
+import java.util.Scanner;
 
 public class Division {
 
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=Integer.valueOf(s.nextLine());
+        int b=Integer.valueOf(s.nextLine());
+        division(a, b);
         
         // Once you have implemented the division method, you can
         // try it out here. Fopr example division(3,5);
@@ -12,4 +16,7 @@ public class Division {
     }
 
     // implement the method here
+    public static void division(int numerator, int denominator){
+        System.out.println(numerator*1.0/denominator*1.0);
+    }
 }
