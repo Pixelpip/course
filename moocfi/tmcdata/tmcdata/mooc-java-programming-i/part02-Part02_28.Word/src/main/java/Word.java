d2c08d83624833023a0b6f19446b44d17266dcb3
Borat560 <athulkamathg5@gmail.com>
2023-07-29 11:50:00 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_28.Word/src/main/java/Word.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_28.Word/src/main/java/Word.java
@@ -5,7 +5,11 @@ public class Word {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+       
+        word();
 
     }
-
+    public static String word(){
+        return "heil";
+    }
 }
