ae2df3adec62007b09139e29fe3cb2c66315a18e
Borat560 <athulkamathg5@gmail.com>
2023-07-29 11:39:01 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -5,7 +5,10 @@ public class NumberUno {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        numberUno();
+    }
+    public static int numberUno(){
+        return 1;
     }
 
 }
