f76a1f70d5a10956a0e29b6380a3bffb7d37b961
Borat560 <athulkamathg5@gmail.com>
2023-07-29 19:09:03 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -3,8 +3,21 @@ public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
         //write some code here
-
-        return -1;
+        if(number1>=number2){
+            if(number1>=number3){
+                return number1;
+            }else{
+                return number3;
+            }
+        }else if(number2>=number3){
+            if(number2>=number1){
+                return number2;
+            }else{
+                return number1;
+            }
+        }else{
+            return number3;
+        }
     }
 
     public static void main(String[] args) {
