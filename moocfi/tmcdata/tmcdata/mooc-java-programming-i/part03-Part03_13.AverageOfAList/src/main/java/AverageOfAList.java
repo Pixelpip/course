458afa70a482b273cb2f5782dae95572c063eb60
Borat560 <athulkamathg5@gmail.com>
2023-08-02 19:25:47 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -11,7 +11,20 @@ public class AverageOfAList {
         // adding them on a list until user gives -1.
         // Then it computes the average of the numbers on the list
         // and prints it.
-        
+        ArrayList<Integer> a=new ArrayList<>();
+        while(true){
+            int input=Integer.valueOf(scanner.nextLine());
+            if(input==-1){
+                break;
+            }
+            a.add(input);
+        }
+        double avg=0;
+        for(Integer i:a){
+            avg=i+avg;
+        }
+        double ag=avg/a.size();
+        System.out.println("Average:"+ag);
         
     }
 }
