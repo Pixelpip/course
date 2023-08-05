ba5842b6907feb0085b3c60f722035d95b3c25de
Borat560 <athulkamathg5@gmail.com>
2023-08-05 17:59:17 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
@@ -5,7 +5,24 @@ public class Sum {
 
     public static void main(String[] args) {
         // Try your method here
-        
+        ArrayList<Integer> numbers=new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        System.out.println(sum(numbers));
+
+        numbers.add(5);
+        numbers.add(1);
+        System.out.println(sum(numbers));
     }
+    public static int sum(ArrayList<Integer> n){
+        int x=0;
+        for(Integer i:n){
+            x=x+i;
+        }
+        return x;
+    }
+
 
 }
