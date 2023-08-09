5d14b9d208d8caf27b0bb6aa3a1192b91f98b285
Borat560 <athulkamathg5@gmail.com>
2023-08-09 23:03:33 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -9,5 +9,12 @@ public class ArrayPrinter {
 
     public static void printNeatly(int[] array) {
         // Write some code in here
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]);
+            if(i<array.length-1){
+                System.out.print(",");
+            }
+            
+        }
     }
 }
