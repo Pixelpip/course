376c210a4d6b4f93e749343e6c2f1b33d8d64ed6
Borat560 <athulkamathg5@gmail.com>
2023-08-09 23:35:58 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -9,7 +9,12 @@ public class Printer {
 
     public static void printArrayInStars(int[] array) {
         // Write some code in here
-        
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i];j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
 }
