0f0b7d8175776831fecd5c517f636edd473fa623
Borat560 <athulkamathg5@gmail.com>
2023-08-10 19:16:06 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
@@ -5,7 +5,12 @@ public class IsItTrue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        String s=scanner.nextLine();
+        if(s.equals("true")){
+            System.out.println("You got it right!");
+        }else{
+            System.out.println("Try again");
+        }
 
     }
 }
