13b836a36637c4902398a0e35201c3b5afe93bf0
Borat560 <athulkamathg5@gmail.com>
2023-08-10 19:24:08 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -5,6 +5,15 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String s=scanner.nextLine();
+        System.out.print("Enter password: ");
+        String p=scanner.nextLine();
+        if((s.equals("alex")&&p.equals("sunshine"))||(s.equals("emma")&&p.equals("haskell"))){
+            System.out.println("You have successfully logged in!");
+        }else{
+            System.out.println("Incorrect username or password!");
+        }
 
     }
 }
