edfd593341d2f786ad2f7fa0936b89cbb6dc5f20
Borat560 <athulkamathg5@gmail.com>
2023-08-10 23:14:44 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,8 +5,15 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        for(int i=0;i<)
         String s=scanner.nextLine();
+        if(s.equals(" ")){
+            System.out.println("halted");
+        }else{
+            String[] p=s.split(" ");
+            for(int i=0;i<p.length;i++){
+                System.out.println(p[i]);
+            }
+        }
 
     }
 }
