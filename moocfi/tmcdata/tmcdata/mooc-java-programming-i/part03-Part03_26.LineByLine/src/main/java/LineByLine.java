e499ec39950f1ea36fa66e472085bae1da645355
Borat560 <athulkamathg5@gmail.com>
2023-08-10 20:16:30 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,6 +5,8 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        for(int i=0;i<)
+        String s=scanner.nextLine();
+
     }
 }
