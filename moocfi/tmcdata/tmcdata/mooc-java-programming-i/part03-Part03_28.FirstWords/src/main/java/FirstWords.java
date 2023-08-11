d5a84a07270d10b4e00467013442f2add58b5680
Borat560 <athulkamathg5@gmail.com>
2023-08-11 20:29:25 +0530

    done while>for

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,14 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        while(true){
+            String s=scanner.nextLine();
+            if(s.equals("")){
+                break;
+            }
+            String[] p=s.split(" ");
+            System.out.println(p[0]);
+        }
 
     }
 }
