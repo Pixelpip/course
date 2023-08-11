d8ac7f1b27361c4c0783178ee21bfbc4c9ae0315
Borat560 <athulkamathg5@gmail.com>
2023-08-11 19:00:52 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,13 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String s=scanner.nextLine();
+        String[] p=s.split(" ");
+        for(int i=0;i<p.length;i++){
+            if(p[i].equals("av")){
+                System.out.println(p[i]);
+            }
+        }
 
     }
 }
