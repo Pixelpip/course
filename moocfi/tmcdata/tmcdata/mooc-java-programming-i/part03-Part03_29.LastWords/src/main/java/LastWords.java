b5e742dffa3de65562cf598c2bdf71e83c25e404
Borat560 <athulkamathg5@gmail.com>
2023-08-11 21:42:02 +0530

    done

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,6 +5,14 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while(true){
+            String s=scanner.nextLine();
+            if(s.equals("")){
+                break;
+            }
+            String[] p=s.split(" ");
+            System.out.println(p[p.length-1]);
+        }
 
 
     }
