108af34c054bbc357cfda972fb59b5ff81a25e6e
Borat560 <athulkamathg5@gmail.com>
2023-08-11 21:56:55 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 public class AgeOfTheOldest {
 
     public static void main(String[] args) {
+        int age=0;
+        String[] p;
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            String s=scanner.nextLine();
+            if(s.equals("")){
+                break;
+            }
+            p=s.split(",");
+        }
 
     }
 }
