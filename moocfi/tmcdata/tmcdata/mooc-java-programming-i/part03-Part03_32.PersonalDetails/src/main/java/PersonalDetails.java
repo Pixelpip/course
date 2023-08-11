ccc17233e30aee63d728984c2be9f48fc385b71b
Borat560 <athulkamathg5@gmail.com>
2023-08-11 23:08:58 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,29 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        double age=0;
+        String[] name;
+        String[] p;
+        String l="";
+        int i=0;
+        while(true){
+            String s=scanner.nextLine();
+            String n=scanner.nextLine();
+            if(s.equals("")||n.equals("")){
+                break;
+            }
+            p=s.split(",");
+            name=n.split(",");
+            age=age+Integer.valueOf(p[1])+Integer.valueOf(name[1]);
+            if(name[0])>p[0]){
+                l=name[0];
+            }else{
+                l=p[0];
+            }
+            i+=2;
+        }
+        System.out.println("Name of oldest is:"+l);
+        System.out.println("Average of birth years:"+(age/i));
 
     }
 }
