a208a0b682a70a19169681d394cc52c20f803759
Borat560 <athulkamathg5@gmail.com>
2023-08-11 22:12:08 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,6 +5,23 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int age=0;
+        int old=0;
+        String name="";
+        String[] p;
+        while(true){
+            String s=scanner.nextLine();
+            if(s.equals("")){
+                break;
+            }
+            p=s.split(",");
+            age=Integer.valueOf(p[1]);
+            if(age>old){
+                old=age;
+                name=p[0];
+            }
+        }
+        System.out.println("Name of oldest is:"+name);
 
 
     }
