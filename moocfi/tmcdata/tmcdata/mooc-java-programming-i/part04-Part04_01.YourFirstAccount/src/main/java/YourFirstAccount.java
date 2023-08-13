cc1dcd6aac853f7008609f9dbf8032bc306470c9
Borat560 <athulkamathg5@gmail.com>
2023-08-13 18:52:59 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account a= new Account("Ben's account", 100.0);
+        a.deposit(20.0);
+        System.out.println(a);
     }
 }
