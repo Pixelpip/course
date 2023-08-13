d04af6b6008cdde698c7925b7149e647891b35ba
Borat560 <athulkamathg5@gmail.com>
2023-08-13 19:00:30 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,11 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account a= new Account("Matthews account", 1000);
+        Account b=new Account("My account", 0);
+        a.withdrawal(100.0);
+        b.deposit(100.0);
+        System.out.println(a);
+        System.out.println(b);
     }
 }
