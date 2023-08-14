4607ea9cfb6449b3406d1e3aacc1fd482985a7eb
Borat560 <athulkamathg5@gmail.com>
2023-08-14 19:35:30 +0530

    1

--- moocfi/tmcdata/tmcdata/mooc-java-programming-i/part04-Part04_05.Whistle/src/main/java/Main.java
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part04-Part04_05.Whistle/src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // This is just an empty main method you can use to test
         // the Whistle class. Try for example:
 
-//        Whistle duckWhistle = new Whistle("Kvaak");
+       Whistle duckWhistle = new Whistle("Kvaak");
 //        Whistle roosterWhistle = new Whistle("Peef");
 //
 //        duckWhistle.sound();
