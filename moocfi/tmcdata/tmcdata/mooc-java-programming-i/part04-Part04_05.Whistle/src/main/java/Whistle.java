8463aec1c0fb918c68e590c122e8cc8025bddeb8
Borat560 <athulkamathg5@gmail.com>
2023-08-14 19:35:27 +0530

    1

---
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part04-Part04_05.Whistle/src/main/java/Whistle.java
@@ -0,0 +1,9 @@
+public class Whistle {
+    pirvate String sound;
+    public Whistle(String whistleSound){
+        this.sound=whistleSound;
+    }
+    public void sound(){
+        System.out.println(Whistle(sound));
+    }
+}
