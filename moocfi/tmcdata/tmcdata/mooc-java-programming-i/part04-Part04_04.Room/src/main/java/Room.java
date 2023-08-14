f6c740ac48e51a2684f4fb94436068e75cf50d98
Borat560 <athulkamathg5@gmail.com>
2023-08-14 19:28:57 +0530

    1

---
+++ moocfi/tmcdata/tmcdata/mooc-java-programming-i/part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,8 @@
+public class Room {
+    private int seats;
+    private String code;
+    public Room(String classcode,int numberOfSeats){
+        this.code=classcode;
+        this.seats=numberOfSeats;
+    }
+}
