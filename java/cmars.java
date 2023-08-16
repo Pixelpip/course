57d7b0efd450ade66c38ef1ebf2bda7d49f31ae2
Atul Kamath <119751753+Borat560@users.noreply.github.com>
2023-08-16 23:17:12 +0530

    Add files via upload
    added

---
+++ java/cmars.java
@@ -0,0 +1,9 @@
+public class cmars {
+    public static void main(String[] args) {
+        int a;
+        byte b;
+        a=100;
+        b=(byte) a; //type conversion 
+        System.out.println(b);
+    }
+}
