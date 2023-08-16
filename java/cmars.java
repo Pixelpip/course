ab3fbdd325411b951bb5f654e27b2bfe525f4def
Atul Kamath <119751753+Borat560@users.noreply.github.com>
2023-08-16 23:16:04 +0530

    Delete java directory
    del

--- java/cmars.java
+++
@@ -1,9 +0,0 @@
-public class cmars {
-    public static void main(String[] args) {
-        int a;
-        byte b;
-        a=100;
-        b=(byte) a; //type conversion 
-        System.out.println(b);
-    }
-}
