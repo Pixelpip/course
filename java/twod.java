8acd4a2704e38b954c358aa10f73e17b1acffe65
Borat560 <athulkamathg5@gmail.com>
2023-08-18 23:18:55 +0530

    1

--- java/twod.java
+++ java/twod.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 public class twod {
     public static void main(String[] args){
         Scanner a=new Scanner(System.in);
-        int two[][]=new int[4][5];
-        for(int i=0;i<4;i++){
-            for(int j=0;j<5;j++){
-                two[i][j]=Integer.valueOf(a.nextLine());
+        int two[][]=new int[2][3];
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                two[i][j]=Integer.valueOf(a.next());
+            }
+        }
+        for(int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(two[i][j]);
             }
         }
     }
