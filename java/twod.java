9db4641dfdec61c3f95e22fe90354d90632c8e68
Borat560 <athulkamathg5@gmail.com>
2023-08-18 23:35:32 +0530

    1

--- java/twod.java
+++ java/twod.java
@@ -10,7 +10,7 @@ public class twod {
         }
         for(int i=0;i<2;i++){
             for(int j=0;j<3;j++){
-                System.out.print(two[i][j]);
+                System.out.println(two[i][j]);
             }
         }
     }
