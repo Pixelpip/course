9dc5899d1b8e9e68605eed47e57f0765694e0646
Borat560 <athulkamathg5@gmail.com>
2023-08-18 22:46:31 +0530

    1

--- java/matrix.java
+++ java/matrix.java
@@ -1,6 +1,8 @@
 public class matrix{
     public static void main(String[] args){
-        int month[];
+        int month[] =new int[5];
+        month[0]=5;
+        System.out.println(month[0]);
         
     }
 }
