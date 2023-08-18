3ca0d6164d4c97d1b5c376870367f3345d96432d
Borat560 <athulkamathg5@gmail.com>
2023-08-18 23:02:35 +0530

    1

--- java/matrix.java
+++ java/matrix.java
@@ -4,7 +4,8 @@ public class matrix{
         month[0]=5;
         System.out.println(month[0]);   //only element assigned a value in the array 
         System.out.println(month[1]);  //all elements of array are zero at start 
-        
+        int year[]={1,2,3,4,5,6};//single line array declaration
+        System.out.println(year[0]);
         
     }
 }
