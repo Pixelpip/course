7461fbad4bff67e512ffda2b9ea069d080411018
Borat560 <athulkamathg5@gmail.com>
2023-08-20 18:21:40 +0530

    1

--- java/sun.java
+++ java/sun.java
@@ -1,5 +1,6 @@
 public class sun {
     public static void main(String[] args){
-        
+        double m[][]={{0,1,2,3},{4,5,6,7}};
+        System.out.println(m[1].length); //print the length of 2nd row i.e the number of columns in 2nd row
     }
 }
