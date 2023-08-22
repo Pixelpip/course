2f03379f475c9ec66a12049d4242324a6a1fb28e
Borat560 <athulkamathg5@gmail.com>
2023-08-22 19:12:11 +0530

    1

--- java/knal.java
+++ java/knal.java
@@ -5,13 +5,16 @@ public class knal{
         System.out.println(a%2==1);
         int x=3;
         int y=6;
+        String s=Integer.toBinaryString(~x&y|x&~y);
+        String n=Integer.toBinaryString(~x);
         System.out.println("binary of x:"+Integer.toBinaryString(x));
         System.out.println("Binary of y:"+Integer.toBinaryString(y));
         System.out.println("x|y:"+Integer.toBinaryString(x|y));
         System.out.println("x&y:"+Integer.toBinaryString(x&y));
         System.out.println("x^y:"+Integer.toBinaryString(x^y));
-        System.out.println("~x&y |x&~y:"+Integer.toBinaryString(~x&y|x&~y));
+        System.out.println("~x&y |x&~y:"+s);
         System.out.println("~x:"+Integer.toBinaryString(~x));
+        System.out.println(n.length());
         
         
         
