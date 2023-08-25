720bea2a97a75669b85d417b556a2a5b9cd4a21b
Borat560 <athulkamathg5@gmail.com>
2023-08-25 19:12:59 +0530

    1

--- java/swi.java
+++ java/swi.java
@@ -1,6 +1,10 @@
 public class swi {
     public static void main(String[] args){
-        int a= (int) System.in.read();
-        
+        int a;
+        int b;
+        for(a=10,b=0;a>b;a--,b++){
+            System.out.println(a+" "+b);
+        }
+
     }
 }
