87724ccb84038311e0d414574def81f84dc14a92
Borat560 <athulkamathg5@gmail.com>
2023-11-07 00:54:10 +0530

    1

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,14 @@
+var rand1=Math.floor(Math.random()*7);
+var rand2=Math.floor(Math.random()*7);
+document.querySelector("img.img1").setAttribute("src","./images/dice"+rand1+".png");
+document.querySelector("img.img2").setAttribute("src","./images/dice"+rand2+".png");
+if (rand1<rand2){
+    document.querySelector("h1").innerHTML="Player 2 wins";
+}
+else if(rand1==rand2){
+    document.querySelector("h1").innerHTML="Draw";
+}
+else{
+    document.querySelector("h1").innerHTML="Player 1 wins";
+}
+
