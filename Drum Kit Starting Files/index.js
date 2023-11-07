70599a45c7ecbe05948e201c2a9375144b34b610
Pixelpip <athulkamathg5@gmail.com>
2023-11-08 00:14:23 +0530

    1

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,9 +1,10 @@
-for (var i=1;i<8;i++){
+for (var i=0;i<8;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",
     function (){
     var audio=new Audio("sounds/tom-1.mp3");
     audio.play();
     console.log(this);
+    this.style.color="black";
    
 })
 
