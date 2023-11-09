e8ba27004c271f559462654885eaa5e46963402b
Pixelpip <athulkamathg5@gmail.com>
2023-11-10 00:51:04 +0530

    1

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,19 +1,44 @@
-var num=document.querySelectorAll(".durm").length;
-for (var i=0;i<num;i++){
+var n=document.querySelectorAll(".durm").length;
+for (var i=0;i<n;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",
     function (){
 
         var bih=this.innerHTML;
         switch(bih){
             case "w":
-                var audio=new Audio("sound/ton-1.mp3");
-                audio.play();
+                var t1=new Audio("sounds/tom-1.mp3");
+                t1.play();
                 break;
             case "a":
-                var audio=new Audio("sound/ton-2.mp3");
-                audio.play();
+                var t2=new Audio("sounds/tom-2.mp3");
+                t2.play();
                 break;
-            case ""
+            case "s":
+                var t3=new Audio("sounds/tom-3.mp3");
+                t3.play();
+                break;
+            case "d":
+                var t4=new Audio("sounds/tom-4.mp3");
+                t4.play();
+                break;
+            case "j":
+                var kick=new Audio("sounds/kick.mp3");
+                kick.play();
+                break;
+            case "j":
+                var snare=new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "j":
+                var crash=new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "l":
+                var bass=new Audio("sounds/bass.mp3");
+                bass.play();
+                break;
+            default:
+                console.log(bih);
         }
    
    
