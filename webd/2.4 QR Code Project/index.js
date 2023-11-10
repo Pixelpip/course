3af35fc916e9da845cb32790db797f6e057274f8
Pixelpip <athulkamathg5@gmail.com>
2023-11-10 23:00:46 +0530

    1

--- webd/2.4 QR Code Project/index.js
+++ webd/2.4 QR Code Project/index.js
@@ -3,7 +3,9 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+import qr from "qr-image";
 import inquirer from 'inquirer';
+import fs from "fs";
 
 inquirer
   .prompt([
@@ -13,6 +15,10 @@ inquirer
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
+    const url=answers.url;
+   
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(require('fs').createWriteStream('qr.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -21,3 +27,6 @@ inquirer
       // Something else went wrong
     }
   });
+
+
+  
