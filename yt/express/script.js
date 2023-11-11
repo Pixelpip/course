fb76096ff88119e521f7d4c1df97593de0ce95ee
Pixelpip <athulkamathg5@gmail.com>
2023-11-11 23:41:17 +0530

    1

---
+++ yt/express/script.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('Hello Profile!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
