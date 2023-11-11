a1b8c6f3a83628656d0a3fa587e692b79a3e7e51
Pixelpip <athulkamathg5@gmail.com>
2023-11-12 00:57:27 +0530

    1

--- yt/express/script.js
+++ yt/express/script.js
@@ -10,7 +10,7 @@ app.use( function(req, res, next) {
 app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.get('/profile/:username', (req, res) => {
