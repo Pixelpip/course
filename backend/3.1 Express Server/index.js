860ccccc9bb0e01f4ce51182d562a01fcabb8b32
Pixelpip <athulkamathg5@gmail.com>
2023-11-11 17:38:29 +0530

    1

--- backend/3.1 Express Server/index.js
+++ backend/3.1 Express Server/index.js
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("hello,worldo!");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
