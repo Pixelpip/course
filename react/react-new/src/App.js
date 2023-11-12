584005022b7d8664f6263ec851ad813b2181666c
Pixelpip <athulkamathg5@gmail.com>
2023-11-12 20:23:55 +0530

    1

--- react/react-new/src/App.js
+++ react/react-new/src/App.js
@@ -1,57 +1,56 @@
-import {useState,React,useEffect} from 'react';
-import './App.css';
-import SearchIcon from './search.svg';
-import MovieCard from './MovieCard';
-//omdb api key: 1cab1ad1
-const API_URL='http://www.omdbapi.com?apikey=1cab1ad1';
-const movie1={
-    
-        "Title": "Spiderman in Cannes",
-        "Year": "2016",
-        "imdbID": "tt5978586",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
-      
-}
+import React, { useState, useEffect } from "react";
 
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
 
-const App=()=>{
-    const [movies, setMovies] = useState([]);
-    const searchMovies=async(title)=>{
-        const response=await fetch(`${API_URL}&s=${title}`);
-        const data=await response.json();
-        console.log(data.Search);
-    }
-    useEffect(()=>{
-       searchMovies('Spiderman');
-    },[]);
-    return(
-      
-        <div className='app'>
-           <h1>MovieLand</h1>
-           <div className='search'><input placeholder='Search movies' value="Superman" onChange={
-            ()=>{}
-           }/>
-           <img src={SearchIcon}
-           alt="search"
-           onClick={()=>{}}/></div> 
-           {
-            movies?.length>0
-            ? (
-                <div className='container'>
-                {movies.map((movie)=>()}/>
-               </div> 
-            ): (
-                <div className='empty'>
-                    <h2>Not found</h2>
-            )
-           }
-           <div className='container'>
-            <MovieCard movie1={movies[0]}/>
-           </div>
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-       
-    );
-}
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- react/react-new/src/MovieCard.jsx
+++ react/react-new/src/MovieCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-const MovieCard=({movie1})=>{
-    return(
-        <div className='movie'>
-                <div>
-                    <p>{movie1.Year}</p>
-                </div>
-                <div>
-                    <img src={movie1.poster!== 'N/A'? movie1.poster:'https://via.placeholder.com/400' } alt={movie1.title}/>
-                </div>
-                <div>
-                    <span>{movie1.Type}</span>
-                    <h3>{movie1.Title}</h3>
-                </div>
-            </div>
-           
-    );
+
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  return (
+    <div className="movie" key={imdbID}>
+      <div>
+        <p>{Year}</p>
+      </div>
+
+      <div>
+        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+      </div>
+
+      <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+      </div>
+    </div>
+  );
 }
-export default MovieCard
+
+export default MovieCard;
