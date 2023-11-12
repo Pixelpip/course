909009fecfcee486a3127fb30a55298fe056a5d0
Pixelpip <athulkamathg5@gmail.com>
2023-11-12 19:39:12 +0530

    1

--- react/react-new/src/App.js
+++ react/react-new/src/App.js
@@ -1,8 +1,15 @@
 import {React,useEffect} from 'react';
 //omdb api key: 1cab1ad1
-
+const API_URL='http://www.omdbapi.com?apikey=1cab1ad1';
 const App=()=>{
-    const APT_URL='http://www.omdbapi.com?apikey=1cab1ad1';
+    
+    useEffect(()=>{
+        const searchMovies=async(title)=>{
+            const response=await fetch(`${API_URL}&s=${title}`);
+            const data=await response.json();
+            console.log(data);
+        }
+    })
     return(
       
         <h1>Default</h1>
