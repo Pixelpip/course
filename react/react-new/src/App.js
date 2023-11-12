30e4b5f1daef3467c919da84e0ec01f5fb4a6289
Pixelpip <athulkamathg5@gmail.com>
2023-11-12 19:34:39 +0530

    1

--- react/react-new/src/App.js
+++ react/react-new/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import {React,useEffect} from 'react';
+//omdb api key: 1cab1ad1
 
 const App=()=>{
+    const APT_URL='http://www.omdbapi.com?apikey=1cab1ad1';
     return(
-        
+      
         <h1>Default</h1>
        
     );
