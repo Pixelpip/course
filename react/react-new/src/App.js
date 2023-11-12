a42e29f401cb66df7d23dbff15e205a6670031e8
Pixelpip <athulkamathg5@gmail.com>
2023-11-12 19:16:47 +0530

    1

--- react/react-new/src/App.js
+++ react/react-new/src/App.js
@@ -1,19 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
-const P1 = (props) => {
-  
-  return(
-  < >
-  <h1>Hello {props.name}</h1>
-  <h2>age:{props.age}</h2>
-  
-  </>
-)};
 
 
 const App=()=> {
   const [counter, setcounter] = useState(0);
+  useEffect(()=>{
+    setcounter(100);
+  },[]);
   return (
     <div className="App">
       <button onClick={()=> setcounter((prevCount=> prevCount-1))}>-</button>
